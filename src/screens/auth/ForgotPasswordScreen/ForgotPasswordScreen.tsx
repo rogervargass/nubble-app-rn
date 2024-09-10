d99a09eda84f6ef004b254555abf9e48d13c7c73
Roger Vargas <rogerv@abcbrasil.com.br>
2024-09-10 18:21:55 -0300

    feat: create ForgotPasswordScreen

---
+++ src/screens/auth/ForgotPasswordScreen/ForgotPasswordScreen.tsx
@@ -0,0 +1,48 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import React from 'react';
+import {Button} from '../../../components/Button/Button';
+import {Screen} from '../../../components/Screen/Screen';
+import {Text} from '../../../components/Text/Text';
+import {TextInput} from '../../../components/TextInput/TextInput';
+import {useResetNavigationSuccess} from '../../../hooks/useResetNavigationSuccess';
+import {RootStackParamList} from '../../../routes/Routes';
+
+type ForgotPasswordScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'ForgotPasswordScreen'
+>;
+
+export function ForgotPasswordScreen({navigation}: ForgotPasswordScreenProps) {
+  const {reset} = useResetNavigationSuccess();
+  function onSubmit() {
+    reset({
+      title: 'Enviamos as instruções para seu e-mail',
+      description:
+        'Clique no link enviado no seu e-mail para recuperar sua senha',
+      icon: {
+        name: 'messageRound',
+        color: 'primary',
+      },
+    });
+  }
+
+  return (
+    <Screen canGoBack>
+      <Text preset="headingLarge" mb="s16">
+        Esqueci minha senha
+      </Text>
+
+      <Text preset="paragraphLarge" mb="s32">
+        Digite seu e-mail e enviaremos as instruções para redefinição de senha
+      </Text>
+
+      <TextInput
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        boxProps={{mb: 's40'}}
+      />
+
+      <Button onPress={onSubmit} title="Recuperar senha" />
+    </Screen>
+  );
+}
