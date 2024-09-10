1e033ece8082a002e3c09e65afc7532dc0c3203d
Roger Vargas <rogerv@abcbrasil.com.br>
2024-09-10 10:58:46 -0300

    feat: create SignUpScreen

---
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Button} from '../../../components/Button/Button';
+import {Screen} from '../../../components/Screen/Screen';
+import {Text} from '../../../components/Text/Text';
+import {TextInput} from '../../../components/TextInput/TextInput';
+
+export function SignUpScreen() {
+  return (
+    <Screen canGoBack scrollable>
+      <Text preset="headingLarge" mb="s32">
+        Crie sua conta
+      </Text>
+
+      <TextInput label="Seu username" placeholder="@" boxProps={{mb: 's20'}} />
+
+      <TextInput
+        label="Nome Completo"
+        placeholder="Digite seu nome completo"
+        boxProps={{mb: 's20'}}
+      />
+
+      <TextInput
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        boxProps={{mb: 's20'}}
+      />
+
+      <TextInput
+        label="Senha"
+        placeholder="Digite sua senha"
+        boxProps={{mb: 's48'}}
+      />
+
+      <Button title="Criar uma conta" />
+    </Screen>
+  );
+}
