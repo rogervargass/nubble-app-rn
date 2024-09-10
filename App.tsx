527a225c5f6a7706dcdb43953927ee20e12acba5
Roger Vargas <rogerv@abcbrasil.com.br>
2024-09-10 18:20:49 -0300

    feat: add app stack navigation

--- App.tsx
+++ App.tsx
@@ -1,24 +1,16 @@
-import { ThemeProvider } from '@shopify/restyle';
+import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
-import { Button } from './src/components/Button/Button';
-import { Text } from './src/components/Text/Text';
-import { theme } from './src/theme/theme';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Router} from './src/routes/Routes';
+import {theme} from './src/theme/theme';
 
 function App(): React.JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaView>
-        <View style={{ paddingHorizontal: 24 }}>
-          <Text preset="headingLarge">Olá</Text>
-          <Button title="Primary" mt='s14'/>
-          <Button preset='outline' title="Outline" mt='s14'/>
-          <Button loading title="Entrar" mt='s14'/>
-          <Button disabled title="Primary" mt='s14'/>
-          <Button disabled preset='outline' title="Outline" mt='s14'/>
-        </View>
-      </SafeAreaView>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
