b512366807a1e1cdce8866a48a0d116a0700c6f7
Roger Vargas <rogerv@abcbrasil.com.br>
2024-09-16 13:13:20 -0300

    feat: add PostComment service

---
+++ src/domain/PostComment/postCommentService.ts
@@ -0,0 +1,25 @@
+import {apiAdapter} from '@api';
+import {Page} from '@types';
+
+import {postCommentAdapter} from './postCommentAdapter';
+import {postCommentApi} from './postCommentApi';
+import {PostComment} from './postCommentTypes';
+
+async function getList(
+  postId: number,
+  page: number,
+): Promise<Page<PostComment>> {
+  const postCommentPageAPI = await postCommentApi.getList(postId, {
+    page,
+    per_page: 10,
+  });
+
+  return {
+    data: postCommentPageAPI.data.map(postCommentAdapter.toPostComment),
+    meta: apiAdapter.toMetaDataPage(postCommentPageAPI.meta),
+  };
+}
+
+export const postCommentService = {
+  getList,
+};
