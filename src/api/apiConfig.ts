737c44705c755088d02d5d1790ce10fdfdc770f8
Roger Vargas <rogerv@abcbrasil.com.br>
2024-09-16 13:12:54 -0300

    feat: add PostComment domain

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer MQ.pqyrg5k9J9q88BOVJUdfhIgqxV72HtIp9VGTDVI8qAyf868kJm7aSA3wwIgE',
+      'Bearer MQ.rOt_CYSp4ZTjXtz7PYwKfcImngQqH69bIT8GZ72YFfWY7nOt7E1cz_ADu8-r',
   },
 });
